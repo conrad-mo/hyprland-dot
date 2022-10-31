c15032a4e18378464b6019c74acad5e1d5a34171
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2022-10-31 16:34:52 -0400

    Update script

--- install.sh
+++ install.sh
@@ -4,18 +4,18 @@ echo "Updating system packages"
 pacman -Syu --noconfirm
 pacman -S --needed base-devel --noconfirm
 
-yay -S paru brightnessctl okular waybar-hyprland-git nemo bitwarden-desktop spotify spotify-adblock-git \
-webcord visual-studio-code-bin hyprpaper network-manager-applet cpupower grim-git slurp gtklock wofi alacritty \
-blueman pfetch unimatrix pipes.sh --noconfirm
+yay -S paru brightnessctl okular waybar-hyprland-git nemo bitwarden spotify spotify-adblock-git \
+webcord visual-studio-code-bin hyprpaper-git network-manager-applet cpupower grim-git slurp gtklock wofi alacritty \
+blueman pfetch unimatrix-git pipes.sh --noconfirm
 
 echo "Moving dot files"
-mv hypr/ ~/.config/hypr/
-mv romb.png ~/Pictures
-mv alacritty ~/.config/alacritty
-mv gtklock ~/.config/gtklock
-mv waybar ~/.config/waybar
-mv wofi ~/.config/wofi
-mv cava ~/.config/cava
+cp hypr/ ~/.config/hypr/
+cp romb.png ~/Pictures
+cp alacritty ~/.config/alacritty
+cp gtklock ~/.config/gtklock
+cp waybar ~/.config/waybar
+cp wofi ~/.config/wofi
+cp cava ~/.config/cava
 gsettings set org.cinnamon.desktop.default-applications.terminal exec alacritty
 
 exit
