ecab23f956a5c551ad658611989689d0a76415a7
Conrad Mo <51024076+conrad-mo@users.noreply.github.com>
2022-12-04 10:48:12 -0500

    QOL script fixes (bro why is something always missing from my script :sob:)

--- install.sh
+++ install.sh
@@ -9,10 +9,13 @@ yay -S catch2-git socat paru zsh brightnessctl cava gnome-keyring waybar-hyprlan
 webcord visual-studio-code-bin hyprpaper-git network-manager-applet auto-cpufreq grim-git slurp gtklock wofi alacritty \
 blueman pfetch unimatrix-git pipes.sh spicetify-cli catppuccin-gtk-theme-mocha sddm-git zsh-autosuggestions zsh-syntax-highlighting-git xreader xed \
 xviewer xplayer xdg-desktop-portal-wlr ttf-ubuntumono-nerd ttf-dejavu ttf-bitstream-vera noto-fonts cantarell-fonts \
-otf-font-awesome starship ttf-firacode-nerd --noconfirm
+otf-font-awesome starship ttf-firacode-nerd wlogout --noconfirm
 
 #optional iwgtk light ripgrep bc gojq blueberry
 #spotify spotify-adblock spotify-remove-ad-banner
+
+echo "Removing the shitty intel gpu driver"
+yay -R xf86-video-intel --noconfirm
 
 echo "Switching from bash to zsh"
 chsh -s /usr/bin/zsh
@@ -53,15 +56,15 @@ echo "Cloning SDDM theme"
 git clone https://github.com/catppuccin/sddm.git
 echo "Moving SDDM theme"
 sudo cp -r sddm/src/catppuccin-mocha /usr/share/sddm/themes/
-#echo "Creating symlinks for SDDM"
-#sudo systemctl enable sddm.service
+echo "Creating symlinks for SDDM"
+sudo systemctl enable sddm.service
 echo "Changing sddm resolution"
 sudo cp 10-monitor.conf /etc/X11/xorg.conf.d/
 echo "Removing sddm catppuccin"
 rm -rf sddm
-#echo "Enabling auto-cpufreq"
-#sudo systemctl enable auto-cpufreq
-#sudo systemctl start auto-cpufreq
+echo "Enabling auto-cpufreq"
+sudo systemctl enable auto-cpufreq
+sudo systemctl start auto-cpufreq
 echo "Installing catppuccin zsh syntax highlighting"
 git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
 cp -v zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
@@ -70,7 +73,7 @@ rm -rf zsh-syntax-highlighting
 echo "Getting spotifywm"
 git clone https://github.com/amurzeau/spotifywm.git ~/GitHub/spotifywm
 echo "Building spotifywm"
-make -C ~/Github/spotifywm
+make -C ~/Github/spotifywm/
 echo "Moving starship config"
 cp starship.toml ~/.config/
 echo "Move vscode wayland flags"
@@ -79,9 +82,12 @@ echo "Move wlogout config"
 cp -r wlogout ~/.config/
 echo "Grabbing catppuccin zsh syntax highlighting"
 git clone https://github.com/catppuccin/zsh-syntax-highlighting.git
+mkdir ~/.zsh
 cp -v zsh-syntax-highlighting/themes/catppuccin_mocha-zsh-syntax-highlighting.zsh ~/.zsh/
 echo "Changing gtk to dark mode"
 cp -r gtk-3.0 ~/.config/
+echo "Moving .zshrc"
+cp .zshrc ~/
 echo "Don't forget to enable services for sddm and auto-cpufreq"
 echo "Done script"
 
