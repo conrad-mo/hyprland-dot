a68e08da24fb88bdfab5f1391b3b049645223962
Conrad Mo <51024076+conrad-mo@users.noreply.github.com>
2023-01-07 16:29:17 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,13 @@ sudo pacman -Syu --noconfirm
 sudo pacman -S --needed base-devel --noconfirm
 
 echo "Grabbing packages via yay"
-yay -S catch2-git spotify-edge socat paru zsh brightnessctl cava gnome-keyring waybar-hyprland-git nemo nemo-fileroller nemo-preview nemo-image-converter bitwarden \
+yay -S catch2-git spotify socat paru zsh brightnessctl cava gnome-keyring waybar-hyprland-git nemo nemo-fileroller nemo-preview nemo-image-converter bitwarden \
 webcord xed visual-studio-code-bin hyprpaper-git network-manager-applet auto-cpufreq grim-git slurp gtklock wofi alacritty \
 blueman pfetch unimatrix-git pipes.sh spicetify-cli catppuccin-gtk-theme-mocha sddm-git zsh-autosuggestions zsh-syntax-highlighting-git xreader \
 xviewer xplayer xdg-desktop-portal-hyprland-git ttf-ubuntumono-nerd ttf-dejavu ttf-bitstream-vera noto-fonts cantarell-fonts \
 otf-font-awesome starship ttf-firacode-nerd wlogout github-cli ttf-twemoji spotify-adblock spotify-remove-ad-banner protonvpn-gui --noconfirm
 
 #optional iwgtk light ripgrep bc gojq blueberry
-#spotify spotify-adblock spotify-remove-ad-banner
 
 echo "Removing the shitty intel gpu driver"
 yay -R xf86-video-intel --noconfirm
