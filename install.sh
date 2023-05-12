d3e39cb846de6040f7b77b2d3aea0d41a2fc0011
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-05-12 17:36:41 -0400

    Script cleanup? probably still dirty

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ yay -S catch2-git spotify socat paru zsh brightnessctl cava gnome-keyring waybar
 webcord xed visual-studio-code-bin hyprpaper-git network-manager-applet auto-cpufreq grim-git slurp gtklock wofi alacritty \
 blueman pfetch unimatrix-git pipes.sh spicetify-cli catppuccin-gtk-theme-mocha sddm-git zsh-autosuggestions zsh-syntax-highlighting-git xreader \
 xviewer xplayer xdg-desktop-portal-hyprland-git ttf-ubuntu-mono-nerd ttf-dejavu ttf-bitstream-vera noto-fonts cantarell-fonts \
-otf-font-awesome starship ttf-firacode-nerd wlogout github-cli ttf-twemoji spotify-adblock spotify-remove-ad-banner protonvpn-gui \
-folder-color-switcher nemo-python-git polkit-kde-agent --noconfirm
+otf-font-awesome starship ttf-firacode-nerd wlogout github-cli ttf-twemoji protonvpn-gui \
+folder-color-switcher nemo-python-git polkit-kde-agent dunst --noconfirm
 
 #optional iwgtk light ripgrep bc gojq blueberry
 
